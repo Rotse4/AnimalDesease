ea1d138f67072002187ce9cdf58ca9503ab7f2c2
Rotse4 <muusyajapheth12@gmail.com>
2024-02-06 10:46:06 +0300

    new

--- lib/home page.dart
+++ lib/home page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,8 +7,28 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      
+    return Scaffold(
+      body: Column(
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 40, left: 15, right: 15),
+          height: 100,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 127, 191, 129)
+          ),
+        child: Row(
+          children: [
+            Container(
+              height: 30,
+              width: 30,
+              color: Colors.amber,
+            )
+          ],
+        ),
+        )
+      ],
+    )
     );
   }
 }
