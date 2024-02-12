d928a5d5f37b278d79f93d0e4a47584572666342
Rotse4 <muusyajapheth12@gmail.com>
2024-02-12 09:34:53 +0300

    added top list view

--- lib/home page.dart
+++ lib/home page.dart
@@ -8,24 +8,71 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       body: Column(
       children: [
         Container(
-          margin: EdgeInsets.only(top: 40, left: 15, right: 15),
+          margin: EdgeInsets.only(top: 30, left: 15, right: 15),
+          padding: EdgeInsets.symmetric(horizontal: 10),
           height: 100,
           width: double.infinity,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 127, 191, 129)
-          ),
+          
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
-              height: 30,
-              width: 30,
-              color: Colors.amber,
-            )
+              height: 42,
+              width: 42,
+              child: Center(
+                child: Text("EK", style: TextStyle(fontSize: 20, color: Colors.orange),)),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 88, 85, 85),
+                borderRadius: BorderRadius.circular(21)
+              ),
+            ),
+
+            Text("Home", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 16,),),
+            Container(
+              height: 42,
+              width: 42,
+              child: Icon(Icons.notifications_outlined, color: Colors.orange,),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 88, 85, 85),
+                borderRadius: BorderRadius.circular(21)
+              ),
+            ),
           ],
         ),
+        ),
+
+        Container(
+          child: Text("Hello Eric", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+        ), 
+
+        Container(
+          height: 150,
+          // color: Colors.greenAccent,
+          child: ListView.builder(
+            itemCount: 10,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index)
+            {
+              return Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Center(child: Text("JK")),
+                  ),
+                  Text("Fever")
+                ],
+              );
+            }),
         )
       ],
     )
