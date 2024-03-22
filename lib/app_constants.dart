5869a8b0864045a6f4804de78499f182505c4b2d
Rotse4 <muusyajapheth12@gmail.com>
2024-03-22 21:30:06 +0300

    sijui

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants{
   static const String APP_NAME = "DBFood";
   static const int APP_VERSION = 1;
   // static const String BASE_URL = "http://172.18.0.42:23020";
-  static const String BASE_URL ="http://10.122.11.142:5000";
+  static const String BASE_URL ="http://192.168.137.94:5000";
   static const String DETECT_URI ="/disease-prediction";
   
 } 
